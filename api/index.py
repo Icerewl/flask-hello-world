c18b1feba9b29d5643e01413f853d5e07d5a1311
ABDULLAH UMUTCAN ORTAKCI <icerewl@gmail.com>
2024-01-10 03:52:30 +0300

    Api changes

--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# A simple route to return a "Hello World" message
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello_world():
+    return jsonify(message='Hello, World!')
 
-@app.route('/about')
-def about():
-    return 'About'
+# A route to return data based on a GET request
+@app.route('/data', methods=['GET'])
+def get_data():
+    sample_data = {'key': 'value'}
+    return jsonify(sample_data)
+
+# A route to receive data based on a POST request
+@app.route('/data', methods=['POST'])
+def post_data():
+    data = request.json
+    # Do something with the data
+    return jsonify(received=True, data=data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
