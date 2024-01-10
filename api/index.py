47a26cc382291ba1a848441c6fc29b94e66e09ac
ABDULLAH UMUTCAN ORTAKCI <icerewl@gmail.com>
2024-01-10 03:58:39 +0300

    Database added

--- api/index.py
+++ api/index.py
@@ -1,24 +1,33 @@
-from flask import Flask, jsonify, request
+import csv
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
-# A simple route to return a "Hello World" message
-@app.route('/')
-def hello_world():
-    return jsonify(message='Hello, World!')
+# Load the CSV data into a list of dictionaries
+with open('ToyotaCorolla.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    car_data = list(csv_reader)
 
-# A route to return data based on a GET request
 @app.route('/data', methods=['GET'])
 def get_data():
-    sample_data = {'key': 'value'}
-    return jsonify(sample_data)
+    index = request.args.get('index', default=None, type=int)
+    if index is not None:
+        try:
+            return jsonify(car_data[index])
+        except IndexError:
+            abort(404)  # Not found if index is out of range
+    return jsonify(car_data)  # Return all data if no index provided
 
-# A route to receive data based on a POST request
-@app.route('/data', methods=['POST'])
-def post_data():
-    data = request.json
-    # Do something with the data
-    return jsonify(received=True, data=data)
+@app.route('/data/search', methods=['GET'])
+def search_data():
+    query = request.args.get('model', default=None, type=str)
+    if query:
+        result = [car for car in car_data if query.lower() in car['Model'].lower()]
+        if result:
+            return jsonify(result)
+        else:
+            abort(404)  # Not found if no match
+    return jsonify(car_data)  # Return all data if no query provided
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
